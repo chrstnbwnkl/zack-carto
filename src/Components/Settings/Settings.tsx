4bf1017612290277190ee143dc65be920c8a3b6b
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2022-12-27 10:05:26 +0100

    settings modal is its own component

---
+++ src/Components/Settings/Settings.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface SettingsProps {
+  onChange: (key: string, value: number | string) => void;
+  settings: string;
+}
+
+const SettingsModal = ({ onChange, settings }: SettingsProps) => {
+  return (
+    <>
+      <input type="checkbox" id="settings-modal" className="modal-toggle" />
+      <label htmlFor="settings-modal" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h2 className="pb-6 text-center text-2xl font-bold text-blue drop-shadow-md">
+            Settings
+          </h2>
+          <div className=" text-blue">
+            <form>
+              <label
+                htmlFor="timeout-input"
+                title="The number of seconds Overpass will take to return a result. 
+                If the query hasn't finished by the end of the timeout, an empty result set 
+                is returned."
+              >
+                Overpass timeout (seconds):{" "}
+              </label>
+              <input
+                id="timeout-input"
+                type="number"
+                className="input-bordered input h-8 w-20"
+                min="1"
+                max="360"
+                value={JSON.parse(settings)?.timeout}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  onChange("timeout", Number(e.target.value));
+                }}
+              />
+            </form>
+          </div>
+        </label>
+      </label>
+    </>
+  );
+};
+
+export default SettingsModal;
